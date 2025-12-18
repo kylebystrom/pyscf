cdbb1db6c3291e81bc58f4ad76d5974d965646d8
Kyle Bystrom <kylebystrom@gmail.com>
2025-12-18 14:48:58 -0500

    try finding segfault

---
+++ .github/workflows/run_check.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+export OMP_NUM_THREADS=4
+export PYTHONPATH=$(pwd):$PYTHONPATH 
+ulimit -s 20000
+
+sudo apt-get -qq install gdb
+
+mkdir -p pyscftmpdir
+echo 'pbc_tools_pbc_fft_engine = "NUMPY+BLAS"' > .pyscf_conf.py
+echo "dftd3_DFTD3PATH = './pyscf/lib/deps/lib'" >> .pyscf_conf.py
+echo "scf_hf_SCF_mute_chkfile = True" >> .pyscf_conf.py
+echo 'TMPDIR = "./pyscftmpdir"' >> .pyscf_conf.py
+
+version=$(python -c 'import sys; print("{0}.{1}".format(*sys.version_info[:2]))')
+pytest pyscf/sgx/ -s --import-mode=importlib pyscf
+gdb -batch -ex=r -ex=where --args pytest pyscf/sgx/ -s --import-mode=importlib pyscf
+
+pytest_status=$?
+
+num_tmpfiles="$(ls -1 pyscftmpdir | wc -l)"
+echo "There are "$num_tmpfiles" leftover temporary files"
+rm -rf pyscftmpdir
+
+# Test fails if pytest failed or if temporary files were left over.
+if test "$num_tmpfiles" -gt 0 || test "$pytest_status" -ne 0; then
+  exit 1
+fi
